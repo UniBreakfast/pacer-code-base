1af5109cffc04f0bdae73810e4876edee5c6da38
unibreakfast <unibreakfast1@gmail.com>
2021-05-03 23:17:46 +0300

    move splitter

--- split-view/SplitView.js
+++ split-view/SplitView.js
@@ -3,15 +3,40 @@ export class SplitView {
     this.parent = parent
     this.options = {...splitViewDefaults, ...options}
     this.render()
+    this.listen()
   }
 
   render() {
     if (!this.el) this.el = buildSplitView(this.options.direction)
     this.parent.append(this.el)
-
     applyInlineStyling(this.el, this.options)
+    this.moveSplitter()
+  }
 
+  listen() {
+    const splitter = this.el.querySelector('splitter')
 
+    splitter.onmousedown = () => {
+      this.el.onmousemove =
+        ({x, y}) => this.moveSplitter(this.options.direction == 'row' ? x : y)
+      this.el.onmouseleave = this.el.onmouseup =
+        () => this.el.onmousemove = null
+    }
+  }
+
+  moveSplitter(pos) {
+    const [sect1, _, sect2] = this.el.children
+    const {direction, padding, gap} = this.options
+    const sectionSpace = padding * -4 - gap +
+      this.el['client' + (direction == "row" ? 'Width' : 'Height')]
+    const startPos = padding + gap/2
+      this.el.getBoundingClientRect()[direction == "row" ? 'left' : 'top']
+    const portion =
+      pos !== undefined ? (pos - startPos) / sectionSpace : this.options.portion
+    const part1 =
+      Math.min(sectionSpace - 12, Math.max(12, sectionSpace * portion))
+    sect1.style.flex = part1
+    sect2.style.flex = sectionSpace - part1
   }
 }
 
@@ -29,7 +54,7 @@ const splitViewDefaults = {
 function buildSplitView(direction) {
   const div = document.createElement('div')
   div.innerHTML = `
-    <splitview style="
+    <splitview ${direction} style="
       width:100%; height:100%; box-sizing: border-box; display:flex;
       flex-direction:${direction};
     ">
@@ -50,9 +75,9 @@ function applyInlineStyling(el, props) {
 
   splitter.style = `
     z-index: 1; display: flex;
-    justify-content: center; align-items: center;
+    justify-content: center; align-items: center; user-select: none;
     ${direction == "row" ? `width: ${gap}px; cursor: col-resize`
-      : `height: ${gap}px; cursor: row-resize`}
+      : `height: ${gap}px; cursor: row-resize`};
   `
   btn.style = `
     width: ${29+borderWidth}px; height: ${29+borderWidth}px;
@@ -61,7 +86,7 @@ function applyInlineStyling(el, props) {
   `
   sect1.style = sect2.style = `
     border: ${borderWidth}px solid ${borderColor};
-    border-radius: ${borderRadius}px;
+    border-radius: ${borderRadius}px; overflow: auto;
   `
   if (padding) el.style.padding = padding + 'px'
   else {
@@ -70,8 +95,4 @@ function applyInlineStyling(el, props) {
     sect2.style.borderWidth = direction == "row" ?
       `0 0 0 ${borderWidth}px` : `${borderWidth}px 0 0 0`
   }
-  const sectionSpace = padding * -4 - gap +
-    el['client' + (direction == "row" ? 'Width' : 'Height')]
-  sect1.style.flex = sectionSpace * portion
-  sect2.style.flex = sectionSpace - sectionSpace * portion
 }
--- split-view/script.js
+++ split-view/script.js
@@ -1,9 +1,16 @@
 import { SplitView } from './SplitView.js'
 
 const options = {
-  direction: 'column',
+  // direction: 'column',
   // padding: 0,
-  borderWidth: 2,
-  borderRadius: 20,
+  // borderWidth: 2,
+  // borderRadius: 20,
 }
 const splitView = new SplitView(document.body, options)
+
+window.splitView = splitView
+
+const [sect1, _, sect2] = splitView.el.children
+
+sect1.innerText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis commodi architecto qui cupiditate adipisci? Quidem facilis vel natus deleniti ex excepturi aperiam esse veritatis delectus deserunt eum accusamus porro qui explicabo, repellat officiis, aliquam animi. Beatae reprehenderit exercitationem, impedit animi nihil nemo veniam minus officiis corporis quisquam, in molestiae fugit perferendis ratione quis reiciendis distinctio sed nobis labore aut blanditiis! Obcaecati, nam, libero alias sint nostrum consequatur doloremque nobis deserunt inventore ab aspernatur, laudantium repellat corrupti hic repellendus commodi fuga similique quos soluta consequuntur facere ex eos neque. Officia dicta ex eius aliquid. Reprehenderit nihil quas, nisi rerum fuga incidunt? Nesciunt debitis magnam possimus ipsa quo accusantium earum asperiores facere distinctio delectus in doloremque dignissimos commodi voluptatem obcaecati eaque temporibus, explicabo perspiciatis dicta quidem excepturi dolore. Cum vero non placeat! Nesciunt nihil sint saepe provident, non maiores et optio laborum dolorem reiciendis quibusdam sunt eos suscipit laudantium dolorum beatae minus ratione? Repellat, doloribus eius? Beatae quis eaque in cum quos officia quisquam explicabo dolor hic libero vel vitae voluptatem, laboriosam, quasi earum, non aut tempore tempora. Cupiditate cum vitae voluptatum aspernatur nisi harum voluptatem, iure voluptatibus quas dolore nesciunt error facilis ex iste quod, vero asperiores culpa, neque ipsum. Excepturi! '.repeat(2)
+sect2.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi similique soluta, error numquam obcaecati non consequatur recusandae, consectetur unde quasi odio illum odit nostrum ipsum iste, rem perferendis. Quasi repellat eos obcaecati beatae laborum eaque enim est. Et hic iure minima quidem? Possimus nesciunt odio cumque unde voluptates magni similique maxime sint debitis dolore inventore, temporibus sed placeat aperiam at corrupti rem sit provident magnam consectetur fugit! Quis explicabo et nemo dolores adipisci consequatur id molestiae sint optio illum! Placeat vel obcaecati rem eum vitae, eligendi sequi suscipit sit dolor debitis, eveniet voluptatem consequuntur optio odio asperiores corporis. Quibusdam aspernatur sapiente magni nulla ab impedit explicabo accusamus, tempore delectus! Expedita, blanditiis cum. Atque fugiat temporibus quod pariatur amet sequi laboriosam! Possimus, quaerat illum? Voluptatibus eligendi vel cumque ex nihil officiis commodi obcaecati enim impedit iusto fugiat libero perferendis in autem numquam, nemo est facere. Officiis dolorem dicta id esse maxime perspiciatis dolor rerum quibusdam vero! Velit pariatur quia nihil possimus. '.repeat(2)
